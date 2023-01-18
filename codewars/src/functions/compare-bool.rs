60e9c1b139ca98be2c5786ca830bfc7995b28a88
Matthew Del Monte <mdel3089@gmail.com>
2023-01-17 22:41:38 -0800

    update to compare boolean function

--- codewars/src/functions/compare-bool.rs
+++ codewars/src/functions/compare-bool.rs
@@ -1,15 +1,20 @@
 // compare two boolean values
 // false = not in love
 // true = in love
+  
 fn lovefunc(flower1: u16, flower2: u16) -> bool {
     let timmy = flower1 % 2;
     let sarah = flower2 % 2;
-    
+
     let mut in_love = true;
-    
-    if timmy + sarah == 0 {
+
+    if timmy == sarah {
         in_love = false
     }
-    
+
     in_love
+}
+
+fn main() {
+  println!("This is the result: {}", loveTest(929, 901));
 }
